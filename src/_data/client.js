65c4ccac168e094a85df5ecc8c6440ed40519cb7
“andrewbramwell <andy@voucherstore.io>
2025-12-15 16:30:55 +0700

    schema

--- src/_data/client.js
+++ src/_data/client.js
@@ -3,7 +3,7 @@ module.exports = {
     "email": "divathaimassageandbeauty@gmail.com",
     "phoneForTel": "+447984601122",
     "phoneFormatted": "079846 01122",
-    "openingHours": "Mon-Sun: 9am-4pm | Sat-Sun: 9am-4pm",
+    "openingHours": "Mon-Sun: 9am-4pm",
     "onlineBooking": "https://divathaimassagebeauty.setmore.com/",
     "address": {
         "lineOne": "The Body-Mind Clinic",
@@ -20,7 +20,50 @@ module.exports = {
         "googleReview": "https://g.page/r/CWMdot-UpYlzEBM/review",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://divathaimassage.co.uk"
+    "domain": "https://www.divathaimassage.co.uk",
+    "schema": {
+        "business": {
+            "@context": "https://schema.org",
+            "@type": "HealthAndBeautyBusiness",
+            "@id": "https://www.divathaimassage.co.uk/#business",
+            "name": "Diva Thai Massage & Beauty",
+            "image": "https://www.divathaimassage.co.uk/assets/svgs/logo.svg",
+            "url": "https://www.divathaimassage.co.uk",
+            "telephone": "+447984601122",
+            "email": "divathaimassageandbeauty@gmail.com",
+            "priceRange": "££",
+            "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "The Body-Mind Clinic, 128 Newport Road",
+                "addressLocality": "Cardiff",
+                "postalCode": "CF24 1DH",
+                "addressCountry": "GB"
+            },
+            "geo": {
+                "@type": "GeoCoordinates",
+                "url": "https://maps.app.goo.gl/xipRfE7JAAxuYr9n7"
+            },
+            "openingHoursSpecification": {
+                "@type": "OpeningHoursSpecification",
+                "dayOfWeek": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+                "opens": "09:00",
+                "closes": "16:00"
+            },
+            "sameAs": [
+                "https://www.facebook.com/profile.php?id=61572882921474"
+            ]
+        },
+        "website": {
+            "@context": "https://schema.org",
+            "@type": "WebSite",
+            "@id": "https://www.divathaimassage.co.uk/#website",
+            "url": "https://www.divathaimassage.co.uk",
+            "name": "Diva Thai Massage & Beauty",
+            "publisher": {
+                "@id": "https://www.divathaimassage.co.uk/#business"
+            }
+        }
+    }
 };
 
 
